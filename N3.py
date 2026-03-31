ff8b478cd76e866ab6cffce1c9c376104a815e5e
busz3 <busz3025@gmail.com>
2026-03-31 20:58:24 -0300

    ai
    ai

---
+++ N3.py
@@ -0,0 +1,11 @@
+# nome = ''
+# while nome != "fim":
+#     nome = input("Digite 'fim'")
+#     print(f'olá {nome}, bem vindo ao laço de repetição')
+# print('saiu do laço')
+
+contador = 0
+while contador < 10:
+    print(f'o valor do contador é {contador}, vamos rodar o laço novamente')
+    contador += 1
+print('saiu do laço')
